8cd0ee09d1f51058346569743c3087bd0f7ab64b
LemmWinks <jomsoutosantiago@gmail.com>
2022-07-27 13:11:51 -0300

    Added Conditionals.java, explained breakpoints
    - added Conditionals.java, fixed it's logic
    - explained how to add breakpoints on eclipse

--- debugging/src/debugging/Breakpoints.java
+++ debugging/src/debugging/Breakpoints.java
@@ -3,6 +3,12 @@ package debugging;
 public class Breakpoints {
 
 	public static void main(String[] args) {
+		
+		// when a debugger is ran, it will go to every breakpoint
+		// and show what it does
+		// to add a breakpoint simply right click on the left side of the java editor
+		// "add breakpoint"
+		// or hold shift + doubleclick
 
 		int number = 5;
 		long bigNumber = 50000000000L;
